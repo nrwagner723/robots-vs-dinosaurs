5818fbfbaa796534f251ce9125a1040f3fc3b8c7
Natalie Wagner <nrwagner723@gmail.com>
2022-05-19 18:59:27 -0500

    imported random attack damage amounts to make it more fair for the robot and dinosaur

--- battlefield.py
+++ battlefield.py
@@ -1,18 +1,19 @@
 from robot import Robot
 from dinosaur import Dinosaur
+import random
 
 class Battlefield():
 
     def __init__(self):
         self.robot = Robot('Greg')
-        self.dinosaur = Dinosaur('Ralph', 70)
+        self.dinosaur = Dinosaur('Ralph', random.randint(65, 85))
 
     def run_game(self):
         self.display_welcome()
         self.display_winner()
 
     def display_welcome(self):
-        print('Welcome to the long-awaited battle between the Robot and the Dinosaur!')
+        print(f'Welcome to the long awaited battle between Robot {self.robot.name} and Dinosaur {self.dinosaur.name}!')
 
     def battle_phase(self):
         while self.dinosaur.health > 0 and self.robot.health > 0:
@@ -24,7 +25,7 @@ class Battlefield():
     def display_winner(self):
         self.battle_phase()
         if self.dinosaur.health == 0:
-            print(f'{self.dinosaur.name} wins! Well done!')
+            print(f'Dinosaur {self.dinosaur.name} wins! Well done!')
         else:
-            print(f'{self.robot.name} wins! Well done!')
+            print(f'Robot {self.robot.name} wins! Well done!')
 
--- robot.py
+++ robot.py
@@ -1,11 +1,12 @@
 from weapon import Weapon
+import random
 
 class Robot():
 
     def __init__(self, name):
         self.name = name
         self.health = 350
-        self.active_weapon = Weapon('Ole Betty', 70)
+        self.active_weapon = Weapon('Ole Betty', random.randint(65, 85))
 
     def attack(self, dinosaur):
         dinosaur.health -= self.active_weapon.attack_power
