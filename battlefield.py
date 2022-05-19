eb16bd2ec076ce866f5861ab56dbe70691d99fdc
Natalie Wagner <nrwagner723@gmail.com>
2022-05-18 22:21:22 -0500

    created all modules and classes based on UML

---
+++ battlefield.py
@@ -0,0 +1,20 @@
+from robot import Robot
+from dinosaur import Dinosaur
+
+class Battlefield():
+
+    def __init__(self):
+        self.robot = Robot()
+        self.dinosaur = Dinosaur()
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        pass
+
+    def battle_phrase(self):
+        pass
+
+    def display_winner(self):
+        pass
---
+++ dinosaur.py
@@ -0,0 +1,9 @@
+class Dinosaur:
+
+    def __init__(self, name, attack_power):
+        self.name = ''
+        self.attack_power = 0
+        self.health = 0
+
+    def attack(self, robot):
+        pass 
