b9ac6aa6951ae83ad362db2f4bcaaf3e4e91016f
Natalie Wagner <nrwagner723@gmail.com>
2022-05-19 19:06:02 -0500

    added a print statement for the robot to display the name of weapon, how much damage dealt and, how much health the dino has left

--- battlefield.py
+++ battlefield.py
@@ -13,11 +13,12 @@ class Battlefield():
         self.display_winner()
 
     def display_welcome(self):
-        print(f'Welcome to the long awaited battle between Robot {self.robot.name} and Dinosaur {self.dinosaur.name}!')
+        print(f'\nWelcome to the long awaited battle between Robot {self.robot.name} and Dinosaur {self.dinosaur.name}!')
 
     def battle_phase(self):
         while self.dinosaur.health > 0 and self.robot.health > 0:
             self.robot.attack(self.dinosaur)
+            print(f'\n{self.robot.name} attacked {self.dinosaur.name} with {self.robot.active_weapon.name} and did {self.robot.active_weapon.attack_power} damage! Dinosaur {self.dinosaur.name} now has {self.dinosaur.health} health left!')
             self.dinosaur.attack(self.robot)
             if self.dinosaur.health == 0 or self.robot.health == 0:
                 break
@@ -25,7 +26,7 @@ class Battlefield():
     def display_winner(self):
         self.battle_phase()
         if self.dinosaur.health == 0:
-            print(f'Dinosaur {self.dinosaur.name} wins! Well done!')
+            print(f'\nDinosaur {self.dinosaur.name} wins! Well done!\n')
         else:
-            print(f'Robot {self.robot.name} wins! Well done!')
+            print(f'\nRobot {self.robot.name} wins! Well done!\n')
 
--- robot.py
+++ robot.py
@@ -11,5 +11,3 @@ class Robot():
     def attack(self, dinosaur):
         dinosaur.health -= self.active_weapon.attack_power
 
-robot_one = Robot('Greg')
-print(robot_one.active_weapon.name)
